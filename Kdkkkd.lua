c0a6f88a6e3e96eebad1bad5abed5202c9c1d04d
Kdjdididb <100611975+Kdjdididb@users.noreply.github.com>
2022-03-02 20:53:40 +0300

    Kdkkkd.lua

--- Kdkkkd.lua
+++ Kdkkkd.lua
@@ -9775,7 +9775,7 @@ Redis:del(TheKdkkkd..'Kdkkkd:Texting:DevTheKdkkkd')
 return LuaTele.sendText(msg_chat_id,msg_id,'᥀︙ تم حذف كليشه المطور')
 end
 if text == 'مبرمج سورس' or text == 'مبرمج السورس' or text == 'المبرمج' then  
-local UserId_Info = LuaTele.searchPublicChat("Zxooxf")
+local UserId_Info = LuaTele.searchPublicChat("eiyil")
 if UserId_Info.id then
 local UserInfo = LuaTele.getUser(UserId_Info.id)
 local InfoUser = LuaTele.getUserFullInfo(UserId_Info.id)
